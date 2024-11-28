2c145421da37ae772b149ae909a518555299c01f
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2024-11-28 18:56:37 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mzhitnik <mzhitnik@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 12:40:45 by mzhitnik          #+#    #+#             */
-/*   Updated: 2024/11/22 13:32:58 by mzhitnik         ###   ########.fr       */
+/*   Updated: 2024/11/28 14:03:04 by mzhitnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
+#include <unistd.h> 
+#include <stdint.h>
 
 # ifndef	BUFFER_SIZE
 # define	BUFFER_SIZE 3
 #endif
 
-size_t	ft_strlen(const char *s);
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	*ft_calloc(size_t num, size_t size);
-char	*read_from_file(char *big_buffer, int fd);
-char	*ft_current_line(char *big_buffer, char **line);
+void	read_from_file(char **big_buffer, int fd);
+void	ft_line_res(char **big_buffer, char **line, int buff_len, int line_len);
 
 #endif
