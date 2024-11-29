00f899cefcbe7832f2f9044a30a74ed56703f33b
mzhitnik <119492493+MariiaZhytnikova@users.noreply.github.com>
2024-11-29 19:51:44 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: mzhitnik <mzhitnik@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 12:38:39 by mzhitnik          #+#    #+#             */
-/*   Updated: 2024/11/28 18:48:31 by mzhitnik         ###   ########.fr       */
+/*   Updated: 2024/11/29 19:41:04 by mzhitnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h" // add this: -D BUFFER_SIZE=(your number)
+#include <stdio.h> // DELETE ME
 
 char	*get_next_line(int fd)
 {
@@ -19,12 +20,12 @@ char	*get_next_line(int fd)
 	int			line_len;
 	int			buff_len;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	if (!big_buffer)
 		big_buffer = ft_calloc(1, sizeof (char));
-			if (!big_buffer)
-				return (NULL);
+	if (!big_buffer)
+		return (NULL);
 	if (!ft_strchr(big_buffer, '\n'))
 		read_from_file(&big_buffer, fd);
 	if (!big_buffer || !big_buffer[0])
@@ -34,14 +35,15 @@ char	*get_next_line(int fd)
 	}
 	line_len = 0;
 	buff_len = 0;
-	while (big_buffer[line_len] && big_buffer[line_len++] != '\n')
-	while (big_buffer[buff_len++])
+	while (big_buffer[line_len] && big_buffer[line_len] != '\n')
+		line_len++;
+	while (big_buffer[buff_len] != '\0')
+		buff_len++;
 	line = NULL;
-	ft_line_res(&big_buffer, &line, buff_len, line_len - 1);
+	ft_line_res(&big_buffer, &line, buff_len, line_len); // I can save one line wuth return
 	return (line);
 }
-
-//////// MAIN //////////////////////////////////////
+/*//////// MAIN //////////////////////////////////////
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
@@ -66,7 +68,10 @@ int	main(int argc, char** argv)
 	{
 		line = get_next_line(fd);
 		if (line == NULL)
-			break ;
+			{
+				printf("Line is NULL");
+				break ;
+			}
 		count++;
 		printf("[%d]: %s", count, line);
 		free(line);
@@ -74,4 +79,4 @@ int	main(int argc, char** argv)
 	}
 	close(fd);
 	return (0);
-}
+}*/
